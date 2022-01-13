cd21e0907cf8776e4b5e5f8d2b7317023fefc7f2
Felix Levert <felix.levert@gmail.com>
2022-01-12 18:33:58 -0800

    reverted to postgresql, will deploy on digital ocean. working on docker-compose

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+import mysql_connector
 from dotenv import load_dotenv
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,8 +8,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    user = os.environ['POSTGRES_USER']
+    password = os.environ['POSTGRES_PASSWORD']
+    host = os.environ['POSTGRES_HOST']
+    database = os.environ['POSTGRES_DB']
+    port = os.environ['POSTGRES_PORT']
+    SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
     
 
 class ProductionConfig(Config):
